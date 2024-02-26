2ed7e978a40bcc63d7c7f93adc05d4bbaad54975
Computing2025 <ozk8000@gmail.com>
2024-02-26 23:01:15 +0300

    Add project files.

---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class BrandManager : IBrandService
+{
+    private readonly IBrandDal _brandDal;
+
+    public BrandManager(IBrandDal brandDal)
+    {
+        _brandDal = brandDal;
+    }
+
+    public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+    {
+        // burada business rules işletilir...
+        // eğer şart olmazsa boşuna nesne yaratmayız
+
+        Brand brand = new()
+        {
+            Name = createBrandRequest.Name,
+            CreatedDate = DateTime.Now
+        };
+
+        _brandDal.Add(brand);
+
+        CreatedBrandResponse createdBrandResponse = new ();
+        createdBrandResponse.Name = brand.Name;
+        createdBrandResponse.Id = 4;
+        createdBrandResponse.CreatedDate = brand.CreatedDate;
+
+        return createdBrandResponse;
+    }
+
+    public List<GetAllBrandResponse> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
+
